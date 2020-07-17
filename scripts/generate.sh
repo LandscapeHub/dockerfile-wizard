a5d7db37869230310ccf8d7c42c342c30e42397f
zachpflederer <zach@fastmodeltechnologies.com>
2020-07-17 12:59:07 -0500

    use machine argument

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -3,13 +3,9 @@
 echo "FROM buildpack-deps:$(awk -F'_' '{print tolower($2)}' <<< $LINUX_VERSION)"
 
 echo "RUN apt-get update"
-echo "curl -fsSL https://get.docker.com -o get-docker.sh"
-echo "sh get-docker.sh"
 
 if [ ! -e $RUBY_VERSION_NUM ] ; then
     echo "RUN apt-get install -y libssl-dev && wget http://ftp.ruby-lang.org/pub/ruby/$(awk -F'.' '{ print $1"."$2 }' <<< $RUBY_VERSION_NUM)/ruby-$RUBY_VERSION_NUM.tar.gz && \
-    tar -xzvf ruby-$RUBY_VERSION_NUM.tar.gz && \
-    cd ruby-$RUBY_VERSION_NUM/ && \
     ./configure && \
     make -j4 && \
     make install && \
